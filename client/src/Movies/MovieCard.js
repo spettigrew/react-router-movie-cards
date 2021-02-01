b278df3bf73ee448049a4375dd0c389c5c3314e6
spettigrew <sara.pettigrew5@gmail.com>
2021-02-01 15:13:19 -0700

    Added card click handler to component

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function MovieCard (props) {
-  return;
+export default function MovieCard ({movie, handleMovieClick}) {
+  const {id} = useParams();
+  const { title, director, metascore, stars } = movie;
+
+  return (
+    <div className="save-wrapper" onClick={() => handleMovieClick(movie.id)}>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        {id && (
+          <>
+            <h3>Actors</h3>
+            {id && stars.map(star => (
+              <div key={star} className="movie-star">
+                {star}
+              </div>
+            ))}
+          <div className="save-button">Save</div>
+          </>
+        )}
+
+      </div>
+    </div>
+  );
 }
